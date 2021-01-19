3daca15e42d5b4cf8ab1cb2d4e0a2ec5c96905ce
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-19 20:51:03 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -5,5 +5,11 @@ $('document').ready(function(){
       location.reload();
      },3000)
   });
-console.log('Loja Carregada');
+  function CriaTB() {
+	var board = JSON.stringify({
+		nameapp:appname,namerecipe:idrecipe,category:category,icon:icon,background:background,img1:img1,img2:img2,img3:img3,img4:img4,desc:desc});
+	TPL.push(board);
+	localStorage.setItem("BOOK", JSON.stringify(BOOK));
+	}
+  console.log('Loja Carregada');
 });
