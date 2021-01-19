7ec9d032708a667d40ff8e840caf71de5f80fe55
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-19 20:17:56 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -1,4 +1,9 @@
 $('document').ready(function(){
   $('textarea').hide();
+  $('.updatestore').click(function(){
+     setTimeout(function(){
+      location.reload();
+     },3000)
+  });
 console.log('Loja Carregada');
 });
