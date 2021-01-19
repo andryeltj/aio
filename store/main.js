27cc52bd10bda6bb65762a97bf67fb751574507f
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-19 21:18:23 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -13,7 +13,7 @@ $('document').ready(function(){
   var appname="", idrecipe="",category="",appicon="",appbackground="",appimg1="",appimg2="",appimg3="",appimg4="",appdesc="";
   function CriaTB() {
 	var board = JSON.stringify({
-		nameapp:appname,namerecipe:idrecipe,category:category,icon:appicon,background:appbackground,img1:appimg1,img2:appimg2,img3:appimg3,img4:appimg4,desc:appdesc});
+	nameapp:appname,namerecipe:idrecipe,category:category,icon:appicon,background:appbackground,img1:appimg1,img2:appimg2,img3:appimg3,img4:appimg4,desc:appdesc});
 	BOOK.push(board);
 	localStorage.setItem("BOOK", JSON.stringify(BOOK));
 	}
