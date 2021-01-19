624664364b9339bc8e712c6fb3c92d6da7784d71
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-19 20:56:57 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -5,11 +5,13 @@ $('document').ready(function(){
       location.reload();
      },3000)
   });
+  var appname="", idrecipe="",category="",appicon="",appbackground="",appimg1="",appimg2="",appimg3="",appimg4="",appdesc="";
   function CriaTB() {
 	var board = JSON.stringify({
-		nameapp:appname,namerecipe:idrecipe,category:category,icon:icon,background:background,img1:img1,img2:img2,img3:img3,img4:img4,desc:desc});
+		nameapp:appname,namerecipe:idrecipe,category:category,icon:appicon,background:appbackground,img1:appimg1,img2:appimg2,img3:appimg3,img4:appimg4,desc:appdesc});
 	TPL.push(board);
 	localStorage.setItem("BOOK", JSON.stringify(BOOK));
 	}
+  CriaTB();
   console.log('Loja Carregada');
 });
