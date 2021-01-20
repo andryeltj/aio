3fe58ad8a03d43d6b22b5feba3896d1c161a8729
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-20 20:46:19 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -10,28 +10,40 @@ $('document').ready(function(){
   var BOOK = localStorage.getItem("BOOK");// Recupera os dados armazenados
   BOOK = JSON.parse(BOOK); // Converte string para objeto
   if(BOOK == null) // Caso não haja conteúdo, iniciamos um vetor vazio
-    {BOOK = [];CriaTB();}
+    {BOOK = [];CriaBOOK();}
   var MENU = localStorage.getItem("MENU");// Recupera os dados armazenados
   MENU = JSON.parse(MENU); // Converte string para objeto
   if(MENU == null) // Caso não haja conteúdo, iniciamos um vetor vazio
-    {MENU = [];CriaTB();}
+    {MENU = [];CriaMENU();}
   var appname="", idrecipe="",category="",appicon="",appbackground="",appimg1="",appimg2="",appimg3="",appimg4="",appdesc="",recipename="",nameappmenu="";
-  function CriaTB() {
+  function CriaBOOK() {
 	var board = JSON.stringify({
 	nameapp:appname,namerecipe:idrecipe,category:category,icon:appicon,background:appbackground,img1:appimg1,img2:appimg2,img3:appimg3,img4:appimg4,desc:appdesc});
 	BOOK.push(board);
 	localStorage.setItem("BOOK", JSON.stringify(BOOK));
+  	}
+  function CriaMENU() {
 	var board = JSON.stringify({
 	recipename:recipename,nameappmenu:nameappmenu});
 	MENU.push(board);
 	localStorage.setItem("MENU", JSON.stringify(MENU));
 	}
-  CriaTB();
+  var qtdDisp=parseInt("0");
+  function countDisp(){
+    $("div.book li").each(function(){
+	  qtdDisp=qtdDisp+("1");
+	  checkInDB()
+    });
+  }
+  function checkInDB(){
+	  var indice=qtdDisp;
+	  var ind = JSON.parse(MENU[indice]);
+	  if(ind.recipename==recipename){console.log("Instalado: "+recipename)}else{console.log("Adicionando:"+recipename+", "+nameappmenu)};
+  }
   $("div.list li").each(function(){
-	  alert($(this).children("a:nth-child(1)").text());
-//	  $(this).children("a").each(function(){
-//		alert($(this).text())
-//	  });
+	  recipename=$(this).children("a:nth-child(1)").text();
+	  nameappmenu=$(this).children("a:nth-child(2)").text();
+	  countDisp();
     });
   console.log('Loja Carregada');
 });
