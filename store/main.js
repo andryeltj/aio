f743f2f71e050c5e02a4996f009f0d6cb8e6b7a6
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-20 10:32:36 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -15,12 +15,16 @@ $('document').ready(function(){
   MENU = JSON.parse(MENU); // Converte string para objeto
   if(MENU == null) // Caso não haja conteúdo, iniciamos um vetor vazio
     {MENU = [];CriaTB();}
-  var appname="", idrecipe="",category="",appicon="",appbackground="",appimg1="",appimg2="",appimg3="",appimg4="",appdesc="";
+  var appname="", idrecipe="",category="",appicon="",appbackground="",appimg1="",appimg2="",appimg3="",appimg4="",appdesc="",recipename="",nameappmenu="";
   function CriaTB() {
 	var board = JSON.stringify({
 	nameapp:appname,namerecipe:idrecipe,category:category,icon:appicon,background:appbackground,img1:appimg1,img2:appimg2,img3:appimg3,img4:appimg4,desc:appdesc});
 	BOOK.push(board);
 	localStorage.setItem("BOOK", JSON.stringify(BOOK));
+	var board = JSON.stringify({
+	recipename:recipename,nameappmenu:nameappmenu});
+	MENU.push(board);
+	localStorage.setItem("MENU", JSON.stringify(MENU));
 	}
   CriaTB();
   console.log('Loja Carregada');
