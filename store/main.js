220e7c585c00822f3b1b14a71bcb94234f58489e
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-01-23 01:57:30 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -13,11 +13,8 @@ $('document').ready(function(){
   var stdBK="https://images.all-free-download.com/images/graphiclarge/abstract_green_blue_low_poly_background_vector_illustration_570260.jpg";
   var stdIC="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/mimetypes/application-x-iso9660-appimage.svg"
   var iteml, ctItems="0",ItmClss="";
-  var ElHead1='<div class="pkg_disp '+ItmClss+'" alt="'+ctItems+'" style="background:url(\'';
-  var ElHead2='\') no-repeat center;background-size:auto 100%">';
-  var appic1='<button class="app" style="background:url(\'';
-  var appic2='\') no-repeat center 1.3em;background-size:50% auto"></button>';
-  var ckrecipe="",itmBT="",acorpo;
+  var ckrecipe="",itmBT="",acorpo,secapp,acct="i";
+  var funcao="0";
   
   function ClassyBook(){
 	$('div.book li a:nth-child(1)').addClass('appname');
@@ -48,7 +45,8 @@ $('document').ready(function(){
 	  appimg3=$(this).children("a:nth-child(10)").text();
 	  appimg4=$(this).children("a:nth-child(11)").text();
 	  appdesc=$(this).children("a:nth-child(12)").text();
-      GeraItMenu();
+      if(funcao == "0"){GeraItMenu();};
+      if(funcao == "1"){Identifica();};
     });
   }
   var qtdDisp=parseInt("0");
@@ -57,9 +55,15 @@ $('document').ready(function(){
     $("div.list li").each(function(){
 	  recipenm=$(this).children("a:nth-child(1)").text();
 	  appnm=$(this).children("a:nth-child(2)").text();
-      if (ckrecipe == recipenm){itmBT="<button class='del'>Remover</button>"};
-      if(itmBT == "" || itmBT == "<button class='add'>Instalar</button>")
+      if(funcao=="0"){
+		  if (ckrecipe == recipenm){itmBT="<button class='del'>Remover</button>"};
+		  if(itmBT == "" || itmBT == "<button class='add'>Instalar</button>")
 		    {itmBT="<button class='add'>Instalar</button>"};
+	  }
+	  if(funcao=="1"){
+		  if(ckrecipe == recipenm){acct="a";$('div.appinfo button#seta').removeClass("del");$('div.appinfo button#seta').addclass("del");$('div.appinfo button#seta').html("Remover");}
+		  if(acct=="i"){$('div.appinfo button#seta').removeClass("del");$('div.appinfo button#seta').addclass("add");$('div.appinfo button#seta').html("Instalar");}
+	  }
     });
   }
   if(BOOK == null){ BOOK = [] };
@@ -75,8 +79,12 @@ $('document').ready(function(){
         if (appbg == "."){appbg=stdBK}
         if (appicon == "."){appicon=stdIC}
         ItmClss=appname+" "+idrecipe+" "+friendnm;
+        var ElHead1='<div class="pkg_disp '+ItmClss+'" alt="'+appname+'" style="background:url(\'';
+		var ElHead2='\') no-repeat center;background-size:auto 100%">';
         var ElHead=ElHead1+appbg+ElHead2;
 		var itName="<a class='nameapp'>"+friendnm+"</a>"
+		var appic1='<button class="app" alt="'+appname+'"style="background:url(\'';
+		var appic2='\') no-repeat center 1.3em;background-size:50% auto"></button>';
         var appic=appic1+appicon+appic2;
         ckrecipe=idrecipe;	CkInstall();
         var itBotton="<span>"+itmBT+"</span>"; itmBT="";
@@ -84,11 +92,35 @@ $('document').ready(function(){
         acorpo=acorpo+iteml;ItmClss="";
 	};
   $("article").html("<div class='menu'>"+acorpo+"</div><div class='news'></div>");acorpo="";
+  $("i.infoqtd").html(ctItems+" apps disponíveis.");
   $("div.news").html($("div.newspaper").html());$('div.newspaper').remove();
 //Pesquisa
   $('.searchbar').keyup(function(){
-	  var $(this).class();
-	  $('.pkg_disp').addClass("hide");
-	  $(this).html();
-	  });
+	  $('div.pkg_disp').addClass("hide");
+	  var SCont=$(this).html();
+	  if (SCont == null ){$('div.pkg_disp').removeClass("hide");};
+	  if ($('div.pkg_disp.'+SCont).length) {$('div.pkg_disp.'+SCont).removeClass("hide");};
+  });
+  $('.x').click(function(){$("div.searchbar").html("");$('div.pkg_disp').removeClass("hide");});
+//Seleção de Item
+  function Identifica(){
+	  if ( secapp == appname ){
+		  $("div.appname").html(friendnm);
+		  if(appicon == "."){appicon=stdIC};
+		  $("a.applogo").css({'background-image':"url('"+appicon+"')"});
+		  if(appbg == "."){appbg=stdBK};
+		  $("div.appinfo").css({'background-image':"url('"+appbg+"')"});
+		  if(appdesc == null || appdesc > "" || appdesc == ".")
+		    {appdesc=$.base64.decode(appdesc);}
+		    else {appdesc=appname};
+		  $("div.appinfo i").html(appdesc);
+		  $("div.passing").html(category);
+		  }
+  }
+  $("button.app").click(function(){
+	  secapp=$(this).attr('alt');
+	  funcao="1";LerBOOK();
+	  //console.log("Clicou em "+secapp);
+  });
+//fim
 });
