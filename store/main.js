e75f1e48a5c621159813271ac3977823f54952b1
andryeltj <66152615+andryeltj@users.noreply.github.com>
2021-03-09 03:58:06 +0000

    Update main.js

--- store/main.js
+++ store/main.js
@@ -105,7 +105,10 @@ $('document').ready(function(){
 			  $('div.appinfo button.seta').removeClass("add");
 			  $('div.appinfo button.seta').addClass("del");
 			  $('div.appinfo button.seta').attr({'onclick':"location.href='aio:del "+secapp+"'"});
-			  $('div.appinfo button.seta').html("Remover");console.log(recipenm+" Está instalado")};
+			  $('div.appinfo button.seta').html("Remover");console.log(recipenm+" Está instalado");
+			  $('div.appinfo button.openapp').removeClass('hide');
+			  $('div.appinfo button.updtapp').removeClass('hide');
+		  };
 		  	  $('div.appinfo button.openapp').attr({'onclick':"location.href='aio:run "+secapp+"'"});
 		  if(acct=="i"){
 			  $('div.appinfo button.seta').removeAttr('href');
